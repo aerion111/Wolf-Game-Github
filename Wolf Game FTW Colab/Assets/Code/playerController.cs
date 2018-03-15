92bdf6c7b0b30a9424cfc0628d50d12e7cb48463
Alexander B <alexander_birkeland@hotmail.com>
2018-03-15 15:53:18 +0100

    Update, working shooting.

--- Wolf Game FTW Colab/Assets/Code/playerController.cs
+++ Wolf Game FTW Colab/Assets/Code/playerController.cs
@@ -8,12 +8,15 @@ public class playerController : MonoBehaviour {
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public string scoreText; // They were using 'Text' instead of string here, but it gave an error.
+
+    private int Points;
 
     // Use this for initialization
     void Start () {
-
-		
-	}
+        Points = 0;
+        scoreText = "score:" + Points; // When using Text before, they used .text here.
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -37,5 +40,12 @@ public class playerController : MonoBehaviour {
 
         // Destroy the bullet after 3 seconds
         Destroy(bullet, 3.0f);
+    }
+
+    public void AddPoints(int amount) // This might get moved to a score-screen script instead, but for now this is the most intuitive of the currently-existing places for it.
+    {
+        Points = Points + amount;
+        scoreText = "score:" + Points;
+        print("Score:" + Points); // No score screen yet, so just throwing it into the log instead.
     }
 }
---
+++ Wolf Game FTW Colab/Assets/Code/targetController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetController : MonoBehaviour {
+
+    public GameObject targetCorpse; // What to replace the wolf, or other target, with on hit.
+    public int targetValue; // How many points you get for shooting the target.
+    public playerController manager; // Change this to public PointsManager, or such, if you separate it out later.
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet") // Should probably have used lowercase, but I didn't when making the tag so I can't do so here.
+        {
+            Destroy(other.gameObject); // Destroy the bullet - mostly because it was in the code given by the teacher, but we might as well.
+            Instantiate(targetCorpse, transform.position, transform.rotation); // Spawn the corpse.
+            // Do any special effect stuff, like blood-splatter or such. We should do SOMETHING, it feels underwhelming at the moment.
+            print("Wolf hit, worth " + targetValue); // Debug stuff.
+            manager.AddPoints(targetValue);
+            Destroy(this.gameObject); // Finish by destroying the 'live' enemy/Target.
+        }
+    }
+}
